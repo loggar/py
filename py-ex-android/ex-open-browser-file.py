2420d6c2bf797c3f5442c6000f5a27479b0bd59d
charly.lee <webnl@DT-Charly.koi.local>
2018-08-16 09:51:41 +1000

    feat (android) concepts

---
+++ py-ex-android/ex-open-browser-file.py
@@ -0,0 +1,4 @@
+from android import Android
+
+droid = Android()
+droid.webViewShow('file:///sdcard/py/index.html')
---
+++ py-ex-android/ex-open-browser-url.py
@@ -0,0 +1,4 @@
+from android import Android
+
+droid = Android()
+droid.webViewShow("https://google.com")
