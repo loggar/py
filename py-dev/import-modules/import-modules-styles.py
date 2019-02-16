78cc2a4a9952ef9e097d2bd39bed4c5f498f8714
charly.lee <webnl@DT-Charly.koi.local>
2019-02-17 03:18:30 +1100

    feat (import)

---
+++ py-dev/import-modules/import-modules-styles.py
@@ -0,0 +1,18 @@
+"""Illustration of good import statement styling.
+
+Note that the imports come after the docstring.
+
+"""
+
+# Standard library imports
+import datetime
+import os
+
+# Third party imports
+from flask import Flask
+from flask_restful import Api
+from flask_sqlalchemy import SQLAlchemy
+
+# Local application imports
+from local_module import local_class
+from local_package import local_function
