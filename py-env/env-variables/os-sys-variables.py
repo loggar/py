6c3107e1bed02f59d83e3ee064c5ff57e1285470
charly.lee <webnl@DT-Charly.koi.local>
2019-02-22 00:18:47 +1100

    feat (system variables)

--- py-env/env-variables/os-sys-variables.py
+++ py-env/env-variables/os-sys-variables.py
@@ -1,11 +1,10 @@
 import os
 
-if 'HOME' in os.environ:
-    print(os.environ.get('HOME', 'BAD-VALUE'))
-else:
-    print('not in system variables: HOME')
+process_env = 'development'
 
 if 'NODE_ENV' in os.environ:
-    print(os.environ.get('NODE_ENV', 'BAD-VALUE'))
+    process_env = os.environ.get('NODE_ENV', 'development')
 else:
     print('not in system variables: NODE_ENV')
+
+print('process_env: %s' % process_env)
