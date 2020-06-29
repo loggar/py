7d7b7a04eb9c7569a1326b8a8729606db27de448
Charly LEE <charly.loggar@gmail.com>
2020-06-29 15:31:32 +1000

    feat (data class)

---
+++ py-core/class-object/data-classes.py
@@ -0,0 +1,26 @@
+from dataclasses import dataclass
+
+"""
+Since version 3.7, Python offers data classes. There are several advantages over regular classes or other alternatives like returning multiple values or dictionaries:
+
+- a data class requires a minimal amount of code
+- you can compare data classes because __eq__ is implemented for you
+- you can easily print a data class for debugging because __repr__ is implemented as well
+- data classes require type hints, reduced the chances of bugs
+"""
+@dataclass
+class Card:
+    rank: str
+    suit: str
+
+
+card = Card("Q", "hearts")
+
+print(card == card)
+# True
+
+print(card.rank)
+# 'Q'
+
+print(card)
+Card(rank='Q', suit='hearts')
